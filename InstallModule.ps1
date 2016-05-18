cc06d65334577043488fcf0f0a0c51350801b003
glaisne <glaisne@gmail.com>
2016-05-18 13:22:30 -0400

    Created InstallModule

---
+++ InstallModule.ps1
@@ -0,0 +1,10 @@
+﻿$ModuleName   = "ADTools"
+$ModulePath   = "C:\Program Files\WindowsPowerShell\Modules"
+$TargetPath = "$($ModulePath)\$($ModuleName)"
+
+if(-Not(Test-Path $TargetPath))
+{
+    mkdir $TargetPath | out-null
+}
+
+Copy-Item -Verbose -Path "$pwd\ADTools.psm1" -Destination "$($TargetPath)\ADTools.psm1"
