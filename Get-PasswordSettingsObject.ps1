d36429b1354a208e045944be84e1d211e07afe08
glaisne <glaisne@gmail.com>
2016-08-05 18:56:53 -0400

    working on get-passwordsettingsobject. never really got started with it.

---
+++ Get-PasswordSettingsObject.ps1
@@ -0,0 +1,25 @@
+﻿<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Get-PasswordSettingsObject
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(ParameterSetName = "Named")]
+        [string[]] $Name
+    )
+
+    foreach ($N in $Name)
+    {
+        
+    }
+    
+}
