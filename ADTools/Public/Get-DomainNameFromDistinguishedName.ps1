45c7c20970119c7bb87fb903c22851a817204c98
glaisne <glaisne@gmail.com>
2017-03-20 17:16:54 -0400

    Added Get-DomainNameFromDistinguishedName.ps1

---
+++ ADTools/Public/Get-DomainNameFromDistinguishedName.ps1
@@ -0,0 +1,26 @@
+﻿<#
+.Synopsis
+   Gets the name of the domain from a distinguishedname property
+.DESCRIPTION
+   Gets the name of the domain from a distinguishedname property
+.EXAMPLE
+   get-DomainNameFromDistinguishedNAme $(get-aduser gene.laisne).distinguishedName
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function get-DomainNameFromDistinguishedName
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $DistinguishedName
+    )
+
+    Foreach ($Dn in $DistinguishedName)
+    {
+        ($Dn.split(',') | ? {$_ -match "DC="}).replace('DC=','') -join '.'
+    }
+}
